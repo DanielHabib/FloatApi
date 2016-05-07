254adeaa0550dc4158d887f9cf4b609256133aa0
DanielHabib <d.g.habib7@gmail.com>
2016-05-07 19:49:22 -0400

    Set up Class Based Rendering

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 use Doctrine\Common\EventManager;
 use League\Container\ContainerInterface;
-use Refinery29\Piston\Piston;
 use Whoops\Run;
-use Refinery29\Piston\ApiResponse as Response;
-use Refinery29\Piston\Request;
 use FloatApi\Controller\SimpleController;
 //use ExampleController;
+
 use Refinery29\Piston\Router\RouteGroup;
 //$response->setResult()
 
 require 'vendor/autoload.php';
+require_once 'src/Controller/SimpleController.php';
+
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -51,25 +51,32 @@ $container->share('request', function () {
 });
 $container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
 
+
 $container->share(TWIG, function(){
     $loader = new Twig_Loader_Filesystem('./templates');
     $twig = new Twig_Environment($loader);
     return $twig;
 });
-
+$container->share(SimpleController::class)
+            ->withArgument(TWIG);
 $route = new League\Route\RouteCollection($container);
 
+
+$route->get('/amp/{id:number}', SimpleController::class . '::renderPage');
 
 $route->map('POST', '/foo', function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container) {
     // Decode Request
     $body = json_decode($request->getBody()->getContents(), true);
+
     /** @var Twig_Environment $twig */
     // Grab Template using TWIG
     $twig = $container->get(TWIG);
     $template = $twig->render(TEMPLATE_SIMPLE_AMP, $body);
+
     // Create File name
     $number = getInc();
     $filename = FILE_NAME_TEMPLATE_PREFIX.sprintf(FILE_NAME_SIMPLE_AMP, $number);
+
     //Create Template
     $file = fopen($filename, "w");
     fwrite($file, $template);
@@ -78,17 +85,25 @@ $route->map('POST', '/foo', function (ServerRequestInterface $request, ResponseI
 
     // Return Response
     $response->getBody()->write($responseJSON);
-    $response->withHeader('Access-Control-Allow-Origin', '*');
-    return $response;
+
+    return $response->withHeader('Access-Control-Allow-Origin', '*')->withHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+});
+$route->map('OPTIONS', '/foo', function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container)  {
+
+    return $response->withHeader('Access-Control-Allow-Origin', '*')->withHeader('Access-Control-Allow-Headers', 'Content-Type');
 });
 
+$route->map('GET', '/', function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container)  {
+
+    return $response;
+});
 
 $route->map('GET', '/foo/{id:number}', function (ServerRequestInterface $request, ResponseInterface $response, array $args) use ($container)  {
     $id = $args['id'];
     $twig = $container->get(TWIG);
     $template = $twig->render(sprintf(FILE_NAME_SIMPLE_AMP, $id));
     $response->getBody()->write($template);
-    $response->withHeader('Access-Control-Allow-Origin', '*');
     return $response;
 });
 
--- src/Controller/SimpleController.php
+++ src/Controller/SimpleController.php
@@ -2,20 +2,36 @@
 
 namespace FloatApi\Controller;
 
-use Refinery29\Piston\Request;
-use Refinery29\Piston\ApiResponse as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 class SimpleController
 {
-    public function renderPage(Request $request, Response $response)
-    {
-        $loader = new Twig_Loader_Filesystem('./templates');
-        $twig = new Twig_Environment($loader);
+    /**
+     * @var Twig_Environment
+     */
+    protected $twig;
 
-        $foo = $twig->render('amp.html', array('name' => 'Fabien'));
-        $responseResult = new Response($foo);
-//        $response->setResult()
-        return $responseResult;
+    /**
+     * @param Twig_Environment $twig
+     */
+    public function __construct(Twig_Environment $twig){
+
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function renderPage(Request $request, Response $response, $args = [])
+    {
+        $id = $args['id'];
+        $template = $this->twig->render(sprintf(FILE_NAME_SIMPLE_AMP, $id));
+        $response->getBody()->write($template);
+        return $response;
     }
 }
